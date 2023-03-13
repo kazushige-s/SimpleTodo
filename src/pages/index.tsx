5be08b16cd5e66579e3180927753df7724903120
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-03-13 21:56:08 +0900

    💄 tailwindcssをインストール

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <div className="text-red-500">test</div>
+    </>
+  );
+}
