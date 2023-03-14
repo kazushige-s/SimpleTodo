62f394945878cc459056eb8746856790bcd9654d
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-03-14 21:51:01 +0900

    ✨

--- src/components/ColorfulMessage.tsx
+++ src/components/ColorfulMessage.tsx
@@ -5,15 +5,15 @@ type Props = {
   children: string;
 };
 
-export const ColorfulMessage = (props:Props) => {
-
+export const ColorfulMessage = (props: Props) => {
   // オブジェクトの分割代入
   const { color, children } = props;
 
   console.log(props);
 
   const containtStyle = {
-    color: color,
+    // オブジェクトのキーと変数名が同じ場合は省略できる
+    color,
     fontSize: "18px",
   };
 
