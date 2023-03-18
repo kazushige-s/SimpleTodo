41282c54d1a430208ba5d6f57074984d63ec2a32
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-03-18 14:28:19 +0900

    💄 CSSをまとめた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,32 +27,25 @@ export default function Home() {
         <input
           value={todoText}
           onChange={onChangeTodoText}
-          className="rounded-lg border border-gray-300 bg-gray-50 p-1 py-2.5 text-sm text-gray-900 shadow focus:border-blue-500"
+          className="input-area"
           placeholder=" To enter a To Do"
         />
-        <button
-          onClick={onClickAdd}
-          className="mx-2 my-2 rounded-lg bg-blue-600 p-1 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none"
-        >
+        <button onClick={onClickAdd} className="add-btn">
           ADD
         </button>
       </div>
 
       {/* 未完了のToDo */}
-      <div className="m-2 w-96 rounded-lg bg-gray-200 p-2">
-        <h1 className="my-2 text-center align-middle">Incomplete</h1>
+      <div className="area">
+        <h1 className="h1">Incomplete</h1>
         <ul>
           {incompleteTodos.map((todo) => {
             return (
               <div key={Math.random()} className="flex items-center">
                 <Point size={16} />
-                <li className="m-2 font-medium">{todo}</li>
-                <button className="mx-1 my-1 rounded-lg bg-blue-600 px-2.5 py-1 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none">
-                  Done
-                </button>
-                <button className="mx-1 my-1 rounded-lg bg-blue-600 px-2.5 py-1 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none">
-                  Delete
-                </button>
+                <li className="list">{todo}</li>
+                <button className="btn">Done</button>
+                <button className="btn">Delete</button>
               </div>
             );
           })}
@@ -60,17 +53,15 @@ export default function Home() {
       </div>
 
       {/* 完了のToDo */}
-      <div className="m-2 w-96 rounded-lg bg-gray-200 p-2">
-        <h1 className="my-2 text-center align-middle">Completed</h1>
+      <div className="area">
+        <h1 className="h1">Completed</h1>
         <ul>
           {completeTodos.map((todo) => {
             return (
               <div key={Math.random()} className="flex items-center">
                 <Point size={16} />
-                <li className="m-2 font-medium">{todo}</li>
-                <button className="mx-1 my-1 rounded-lg bg-blue-600 px-2.5 py-1 text-sm font-medium text-white hover:bg-blue-700 focus:outline-none">
-                  Back
-                </button>
+                <li className="list">{todo}</li>
+                <button className="btn">Back</button>
               </div>
             );
           })}
