e44db7519ccc9efb373088559c530337c5f5bd20
Kazushige Shirahama <the.stone.monky.6317@gmail.com>
2023-03-18 15:18:16 +0900

    ✨ 削除機能を実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,33 +3,50 @@ import { Point } from "tabler-icons-react";
 
 export default function Home() {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["To Do1"]);
+  const [incompleteTodos, setIncompleteTodos] = useState(["To Do1", "To Do2"]);
   const [completeTodos, setCompleteTodos] = useState(["To Do2"]);
 
+  //入力エリアに入力された時の処理
   const onChangeTodoText = (e: any) => {
+    //変数todoTextを、関数setTodoTextで更新する
     setTodoText(e.target.value);
   };
 
-  const onClickAdd = () => {
+  //追加ボタンが押された時の処理
+  const onClickAdd = (): void => {
     if (todoText === "") {
       alert("To Doを入力してください");
       return;
     }
-    const newTodos = [...incompleteTodos, todoText];
+    //変数newTodosに配列を分割代入する
+    const newTodos: string[] = [...incompleteTodos, todoText];
+    //変数incompleteTodosを、関数setIncompleteTodosで更新する
     setIncompleteTodos(newTodos);
+    //空文字列にすることで、入力エリアを空にする
     setTodoText("");
+  };
+
+  const onClickDelete = (i: number) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(i, 1);
+    setIncompleteTodos(newTodos);
   };
 
   return (
     <div className="font-zenKurenaido">
       {/* 入力エリア */}
-      <div className="1/2 m-2">
+      <div className="m-2">
         <input
+          //value属性には、入力された値を保持するためのもの
           value={todoText}
+          //onChangeの後には、関数を記述する
           onChange={onChangeTodoText}
           className="input-area"
           placeholder=" To enter a To Do"
         />
+
+        {/* 追加ボタン */}
+        {/* onClickAddが押されると、onClickAdd関数が実行される */}
         <button onClick={onClickAdd} className="add-btn">
           ADD
         </button>
@@ -39,13 +56,19 @@ export default function Home() {
       <div className="area">
         <h1 className="h1">Incomplete</h1>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {/* incompleteTodosをmapで回す */}
+          {incompleteTodos.map((todo, i) => {
             return (
               <div key={Math.random()} className="flex items-center">
                 <Point size={16} />
                 <li className="list">{todo}</li>
+
+                {/* 完了ボタン */}
                 <button className="btn">Done</button>
-                <button className="btn">Delete</button>
+                {/* 削除ボタン */}
+                <button onClick={() => onClickDelete(i)} className="btn">
+                  Delete
+                </button>
               </div>
             );
           })}
